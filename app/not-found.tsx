fe60a1ddfcbb59a45f28b0f70c35c203191e02f4
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-05-15 17:46:27 -0300

    not-found customized

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,10 @@
 export default function NotFound() {
-  return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        404 - Page Not Found
-      </h1>
-      <p className="mb-4">
-       The page you are looking for does not exist.
-      </p>
-    </section>
-  )
+	return (
+		<section>
+			<h1 className="mb-8 text-2xl font-semibold tracking-tighter">404 - Page Not Found</h1>
+			<p className="mb-4">
+				The cosmic object you were looking for has disappeared beyond the event horizon.
+			</p>
+		</section>
+	);
 }
