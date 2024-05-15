c797a091c99b8986f92ccbfca69529e590582a8c
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-05-15 12:19:16 -0300

    post component modified

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -1,36 +1,34 @@
-import Link from 'next/link'
-import { formatDate, getBlogPosts } from 'app/blog/utils'
+import { getAllPlanets } from "app/blog/utils";
+import Link from "next/link";
 
-export function BlogPosts() {
-  let allBlogs = getBlogPosts()
+export function BlogPlanets() {
+	let allPlanets = getAllPlanets();
 
-  return (
-    <div>
-      {allBlogs
-        .sort((a, b) => {
-          if (
-            new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
-          ) {
-            return -1
-          }
-          return 1
-        })
-        .map((post) => (
-          <Link
-            key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
-            href={`/blog/${post.slug}`}
-          >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
-                {formatDate(post.metadata.publishedAt, false)}
-              </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                {post.metadata.title}
-              </p>
-            </div>
-          </Link>
-        ))}
-    </div>
-  )
+	return (
+		<div>
+			{allPlanets
+				.sort((a, b) => {
+					if (a.metadata.ua < b.metadata.ua) {
+						return -1;
+					}
+					return 1;
+				})
+				.map((post) => (
+					<Link
+						key={post.slug}
+						className="flex flex-col space-y-1 mb-4"
+						href={`/blog/${post.slug}`}
+					>
+						<div className="w-full flex flex-row space-x-0 md:space-x-2">
+							<p className="text-[#4338ca] dark:text-[#a5b4fc] w-[50px] tabular-nums">
+								{post.metadata.ua}
+							</p>
+							<p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+								{post.metadata.title}
+							</p>
+						</div>
+					</Link>
+				))}
+		</div>
+	);
 }
