d9a3bd29f655633232858bc7bfa1503c21b34aff
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-05-16 11:50:12 -0300

    color on footer link

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -25,24 +25,24 @@ export default function Footer() {
 			<ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
 				<li>
 					<a
-						className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+						className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100 text-[#4f46e5]"
 						rel="noopener noreferrer"
 						target="_blank"
 						href="https://www.linkedin.com/in/andresfernandez89/"
 					>
 						<LinkedinIcon />
-						<p className="ml-2 h-7">linkedin</p>
+						<p className="ml-2 h-7 text-[#4f46e5]">linkedin</p>
 					</a>
 				</li>
 				<li>
 					<a
-						className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+						className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100 text-[#4f46e5]"
 						rel="noopener noreferrer"
 						target="_blank"
 						href="https://github.com/andresfernandez89"
 					>
 						<GithubIcon />
-						<p className="ml-2 h-7">github</p>
+						<p className="ml-2 h-7 text-[#4f46e5]">github</p>
 					</a>
 				</li>
 			</ul>
